387a96c8d97cf4089600f0957516d0c89011d0aa
DESKTOP-5SU2620\Usuario <leoschuch23@gmail.com>
2023-04-20 16:14:12 -0300

    First steps

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import Container from "./Container";
+
+function Navbar() {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+        <div
+            className="
+            py-4
+            border-b-[2px]
+            "
+        >
+            <Container>
+              aaaa
+            </Container>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
