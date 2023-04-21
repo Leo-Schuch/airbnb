135e0ab9c9965a660ec8a181b8c3e6f644799b62
DESKTOP-5SU2620\Usuario <leoschuch23@gmail.com>
2023-04-21 20:45:16 -0300

    search, dropwdown and modal done.

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,4 +1,9 @@
-import Container from "./Container";
+'use client'
+
+import Container from "../Container";
+import Logo from "./Logo"
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 function Navbar() {
   return (
@@ -10,7 +15,18 @@ function Navbar() {
             "
         >
             <Container>
-              aaaa
+              <div className="
+                flex
+                flex-row
+                items-center
+                justify-between
+                gap-3
+                md:gap-0
+              ">
+                <Logo />
+                <Search/>
+                <UserMenu/>
+              </div>
             </Container>
         </div>
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 export const metadata = {
   title: "Teste",
@@ -19,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
+        <ClientOnly>
+          <Modal actionLabel="Enviar" title="Oi testando" isOpen/>  
+          <Navbar/>
+        </ClientOnly>
         {children}
       </body>
     </html>
