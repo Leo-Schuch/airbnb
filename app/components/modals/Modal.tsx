db4b8a474144718888b7d256f2e26b3acc52b54c
DESKTOP-5SU2620\Usuario <leoschuch23@gmail.com>
2023-04-21 21:23:12 -0300

    sign up working with onOpen and onClose.

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -170,13 +170,13 @@ const Modal: React.FC<ModalProps> = ({
                       label={secondaryActionLabel}
                       onClick={handleSecondaryAction}
                     />
-                  )}
+                    )}
+                    <Button
+                      disabled={disabled}
+                      label={actionLabel}
+                      onClick={handleSubmit}
+                    />
 
-                  <Button
-                    disabled={disabled}
-                    label={actionLabel}
-                    onClick={handleSubmit}
-                  />
                 </div>
               </div>
             </div>
